b6e582f5c19e9ede4c668a90bf4adc0f7d6a59a8
luis122448 <luis122448@gmail.com>
2025-07-21 00:01:07 -0500

    feature: add status enpoint

--- app/routers/metadata_router.py
+++ app/routers/metadata_router.py
@@ -1,8 +1,14 @@
 from fastapi import APIRouter, Depends
 from services.metadata_service import MetadataService
 from schemas.api_response_schema import ApiResponseList
+from schemas.api_response_schema import ApiResponseObject
+from services.status_service import StatusService
+from core.config_manager import ReportConfigManager
 
 router = APIRouter(tags=["Metadata & Dashboard"])
+
+def get_status_service(report_config_manager: ReportConfigManager = Depends(ReportConfigManager)):
+    return StatusService(report_config_manager)
 
 @router.get("/dashboard/total_scheduled_reports", response_model=ApiResponseList)
 async def get_total_scheduled_reports(metadata_service: MetadataService = Depends()):
@@ -15,3 +21,8 @@ async def get_weekly_report_execution_details(metadata_service: MetadataService 
     # Returns a list of all report executions in the last week with their status and details.
     executions = metadata_service.get_weekly_report_execution_details_metadata()
     return ApiResponseList(status=1, message="OK", list=executions)
+
+@router.get("/dashboard/status", response_model=ApiResponseObject)
+async def get_reports_status(report_config_manager: ReportConfigManager = Depends(ReportConfigManager)):
+    status_service = StatusService(report_config_manager)
+    return status_service.get_report_status()
--- app/services/status_service.py
+++ app/services/status_service.py
@@ -1,4 +1,5 @@
 from core.config_manager import ReportConfigManager
+from schemas.api_response_schema import ApiResponseObject
 
 class StatusService:
     def __init__(self, report_config_manager: ReportConfigManager):
@@ -12,14 +13,13 @@ class StatusService:
 
         num_companies = len(report_configs)
         num_reports = sum(len(reports) for reports in report_configs.values())
-        from app.schemas.api_response_schema import ApiResponseObject
 
         return ApiResponseObject(
             status=1.0,
             message="Report status retrieved successfully",
             object={
-                "num_companies_configured": num_companies,
-                "num_reports_configured": num_reports,
+                "companies_configured": num_companies,
+                "reports_configured": num_reports,
                 "company_details": company_details
             }
         )
