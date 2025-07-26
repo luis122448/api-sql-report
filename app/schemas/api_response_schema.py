62d644fb98c65d1cc12fe555ea58bfea8e6e7db4
luis122448 <luis122448@gmail.com>
2025-07-26 14:06:51 -0500

    fix: update api respose schema again x2

--- app/schemas/api_response_schema.py
+++ app/schemas/api_response_schema.py
@@ -2,28 +2,6 @@ from pydantic import BaseModel
 from typing import Optional, Any, List, Dict
 from datetime import datetime
 
-
-class ApiResponseSchema(BaseModel):
-    # status:
-    # 1: "Success"
-    # 1.1: "Success but with warnings"
-    # 1.2: "Success but with errors"
-
-    status: float
-    message: str
-    id_cia: Optional[int] = None
-    timestamp: Optional[str] = None
-
-    model_config = {
-        "json_schema_extra": {
-            "examples": [{
-                "status": "1",
-                "message": "Success",
-            }]
-        }
-    }
-
-
 class ApiResponseObject(BaseModel):
     status: float
     message: str
